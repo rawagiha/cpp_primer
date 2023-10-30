6f5a02475ffe03d15cd21e882d06793f59fd3493
Kohei Hagiwara <kohei.hagiwara@stjude.org>
2023-10-30 13:46:01 -0500

    .

--- chapter_01/section_1.2/exercise_1.6.cpp
+++ chapter_01/section_1.2/exercise_1.6.cpp
@@ -7,7 +7,7 @@ int main()
     std::cin >> v1 >> v2;
     std::cout << "The sum of " << v1; //statement ends here
               
-              //<< " and " << v2;
+              // << " and " << v2; (no left-hand operand for operator <<)
     //add osstream obj as left-hand operand and remove ;
     std::cout << " and " << v2          
               << " is " << v1 + v2 << std::endl;

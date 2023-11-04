5a6df0657746da01924b749f572a74e3a38ba0b3
Kohei Hagiwara <kohei.hagiwara@stjude.org>
2023-11-04 17:46:46 -0500

    day06

--- chapter_02/section_2.2/exercise_2.10.cpp
+++ chapter_02/section_2.2/exercise_2.10.cpp
@@ -10,7 +10,14 @@ int main()
     std::string local_str;
     int local_int;
 
+    std::cout << "global_str and global_int are defined outside a function\n"
+              << " global_str is an empty string and global_int is zero." 
+              << std::endl; 
     std::cout << global_str << " " << global_int << std::endl;
+    
+    std::cout << "local_str and local_int are defined inside a function\n"
+              << " both have undefined values." 
+              << std::endl; 
     std::cout << local_str << " " << local_int << std::endl;
     
 }
---
+++ chapter_02/section_2.2/exercise_2.9.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+   //ilegal
+   //std::cin >> int input_val; 
+   
+   int i = {3.14};
+   std::cout << i << std::endl;
+
+   int _i = 3.14;
+   std::cout << _i << std::endl;
+
+   //ilegal
+   //double salary = wage = 999.9;
+}

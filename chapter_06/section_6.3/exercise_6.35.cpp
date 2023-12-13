377338c5de1eac4b5357d4b01b6bca143bc19487
Kohei Hagiwara <kohei.hagiwara@stjude.org>
2023-12-13 13:34:28 -0600

    day42

--- chapter_06/section_6.3/exercise_6.35.cpp
+++ chapter_06/section_6.3/exercise_6.35.cpp
@@ -6,6 +6,15 @@ int factorial(int val)
         
         //return factorial(--val) * val; prefix decrement(--) will be evaluated earlier than * !!!!! 
         // -> evaluated as factorial(val -1 -1) * val - 1
+        
+        /*
+         *Page139
+         *If you change the value of an operand, don't use that operand elsewhere in the same expression
+         *-> undefined
+         * BAD -> factorial(--val) * val Prefix-- modifies val
+         * Good -> factorial(val - 1) *val
+        */
+        
         return factorial(val - 1) * val;
     return 1;
 }

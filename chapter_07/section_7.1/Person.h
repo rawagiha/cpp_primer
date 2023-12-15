c7ac9eec694d1e7ae1e33e81bbf494968de05402
Kohei Hagiwara <kohei.hagiwara@stjude.org>
2023-12-15 13:18:00 -0600

    day44

--- chapter_07/section_7.1/Person.h
+++ chapter_07/section_7.1/Person.h
@@ -2,13 +2,13 @@
 #define PERSON_H
 
 #include <string>
+#include <iostream>
 
-class Person
+struct Person
 {
     std::string _name;
     std::string _address;
 
-public:
     std::string name() const 
     {
         return _name;
@@ -20,5 +20,16 @@ public:
     } 
 };
 
+std::istream& read(std::istream& is, Person& person)
+{
+    is >> person._name >> person._adress;
+    return is;
+}
+
+std::ostream& print(std::ostream& os, const Person& person)
+{
+    os << person.name() << " " << person.address() << std::endl;
+    return os;
+}
 
 #endif

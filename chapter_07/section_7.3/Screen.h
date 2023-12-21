cb6e63fd6cdc945ab2e222490f61116a47ac5ac8
Kohei Hagiwara <kohei.hagiwara@stjude.org>
2023-12-20 20:24:48 -0600

    day48

--- chapter_07/section_7.3/Screen.h
+++ chapter_07/section_7.3/Screen.h
@@ -2,6 +2,7 @@
 #define SCREEN_N
 
 #include <string>
+#include <iostream>
 
 class Screen
 {
@@ -10,22 +11,54 @@ public:
     Screen() = default;
     Screen(pos ht, pos wd, char c): 
            height(ht), width(wd), contents(ht * wd, c) {}
+    
     char get() const { return contents[cursor]; }
     inline char get(pos ht, pos wd) const;
-    Screen &move(pos r, pos c);
+    
+    Screen& move(pos r, pos c);
+    Screen& set(char);
+    Screen& set(pos, pos, char);
+    Screen& display(std::ostream& os)
+    { 
+            do_display(os);
+            return *this;
+    }
+    const Screen& display(std::ostream& os) const
+    {
+            do_display(os);
+            return *this;
+    }
+          
     void  some_member() const; 
 private:
     pos cursor = 0;
     pos height = 0, width = 0;
     std::string contents;
     mutable size_t access_ctr;
+
+    void do_display(std::ostream& os) const 
+        { os << contents; }
 };
 
 inline 
 Screen& Screen::move(pos r, pos c)
 {
     pos row = r * width;
-    cursor = pow + c;
+    cursor = row + c;
+    return *this;
+}
+
+inline
+Screen& Screen::set(char ch)
+{
+    contents[cursor] = ch;
+    return *this;
+}
+
+inline
+Screen& Screen::set(pos r, pos col, char ch)
+{
+    contents[r * width + col] = ch;
     return *this;
 }
 

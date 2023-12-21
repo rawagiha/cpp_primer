b1cb6e0f954a540e17c1554275d388fef72b1d7b
Kohei Hagiwara <kohei.hagiwara@stjude.org>
2023-12-21 15:49:25 -0600

    day48

---
+++ chapter_07/section_7.3/exercise_7.29.cpp
@@ -0,0 +1,10 @@
+#include "Screen_ex28.h"
+
+int main()
+{
+    Screen my_screen(5, 5, 'X');
+    my_screen.move(4, 0).set('#').display(std::cout);
+    std::cout << "\n";
+    my_screen.display(std::cout);
+    std::cout << "\n";
+}

f3cb645643ecf484562f45e7c1784b6b83f74b8b
Kohei Hagiwara <kohei.hagiwara@stjude.org>
2024-01-02 22:14:33 -0600

    day56

---
+++ chapter_08/section_8.2/exercise_8.6.cpp
@@ -0,0 +1,30 @@
+#include <fstream>
+#include "Sales_data.h"
+
+int main(int argc, char *argv[])
+{
+    std::ifstream input(argv[1]);
+    
+    Sales_data total;
+    if (read(input, total))
+    {
+        Sales_data trans;
+        while (read(input, trans))
+        {
+            if (total.isbn() == trans.isbn())
+                total.combine(trans);
+            else
+            {
+                print(std::cout, total) << std::endl;
+                total = trans;
+            }
+        }
+        print(std::cout, total) << std::endl;
+    }
+    else
+    {
+        std::cerr << "No data?!" << std::endl;
+    }
+    
+    return 0;
+}

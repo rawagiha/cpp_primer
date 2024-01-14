e0a5e31e07b1d4bbb4e90ee6e7177180228c1105
Kohei Hagiwara <kohei.hagiwara@stjude.org>
2024-01-14 14:41:11 -0600

    day65

--- chapter_09/section_9.5/exercise_9.43.cpp
+++ chapter_09/section_9.5/exercise_9.43.cpp
@@ -5,12 +5,11 @@ void f1(std::string& s, const std::string& old_val, const std::string& new_val)
 {
     auto it = s.begin();
     const auto sz = old_val.size();
-    while (it != s.end() - sz)
+    while (it <= s.end() - sz)
     {
         std::string _s(it, it + sz);
         if (_s == old_val)
         {
-            
             it = s.erase(it, it + sz);
             it = s.insert(it, new_val.begin(), new_val.end());
             it += new_val.size();
@@ -24,7 +23,7 @@ void f1(std::string& s, const std::string& old_val, const std::string& new_val)
 
 int main()
 {
-    std::string s = "this is valid thru may 1 to june 7 thru through a.";
+    std::string s = "this is valid thru may 1 to june 7 thru through a.thru";
 
     f1(s, "thru", "through");
     std::cout << s << std::endl;

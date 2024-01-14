2752a3ad9ba4203bee3d68d92dcf4bc755dd0ee8
Kohei Hagiwara <kohei.hagiwara@stjude.org>
2024-01-14 12:26:16 -0600

    day65

---
+++ chapter_09/section_9.5/exercise_9.43.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <iostream>
+
+void f1(std::string& s, const std::string& old_val, const std::string& new_val)
+{
+    auto it = s.begin();
+    const auto sz = old_val.size();
+    while (it != s.end() - sz)
+    {
+        std::string _s(it, it + sz);
+        if (_s == old_val)
+        {
+            
+            it = s.erase(it, it + sz);
+            it = s.insert(it, new_val.begin(), new_val.end());
+            it += new_val.size();
+        }
+        else
+        {
+            ++it;
+        }
+    }    
+}
+
+int main()
+{
+    std::string s = "this is valid thru may 1 to june 7 thru through a.";
+
+    f1(s, "thru", "through");
+    std::cout << s << std::endl;
+}

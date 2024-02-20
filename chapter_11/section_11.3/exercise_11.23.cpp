bdbe47fcc62916a62fe8a6ba4710c5818a8d8bb7
Kohei Hagiwara <kohei.hagiwara@stjude.org>
2024-02-19 22:15:05 -0600

    day88

---
+++ chapter_11/section_11.3/exercise_11.23.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <map>
+
+
+int main()
+{
+    
+    std::multimap<std::string, std::vector<std::string>> family_data = { {"family_a", {"kid_a", "kid_b"}} };
+    
+}

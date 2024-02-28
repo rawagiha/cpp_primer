296be2705b53c2686e0b015d36b37f70d10f4971
Kohei Hagiwara <kohei.hagiwara@stjude.org>
2024-02-27 22:45:47 -0600

    day95

--- chapter_11/section_11.3/exercise_11.35.cpp
+++ chapter_11/section_11.3/exercise_11.35.cpp
@@ -16,7 +16,8 @@ std::map<std::string, std::string> build_map(std::ifstream& map_file)
     while(map_file >> key && std::getline(map_file, value))
     {
         if (value.size() > 1)
-            trans_map[key] = value.substr(1);
+            //trans_map[key] = value.substr(1);
+            trans_map.insert({key, value.substr(1)});
         else
             throw std::runtime_error("no rule for " + key);
     }

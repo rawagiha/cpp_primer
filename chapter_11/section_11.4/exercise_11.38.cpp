d876883c2b101ae59e64581bf8c2ab20d12a88ef
Kohei Hagiwara <kohei.hagiwara@stjude.org>
2024-03-01 11:21:56 -0600

    day98

---
+++ chapter_11/section_11.4/exercise_11.38.cpp
@@ -0,0 +1,69 @@
+#include <string>
+#include <unordered_map>
+#include <fstream>
+#include <sstream>
+#include <stdexcept>
+#include <iostream>
+
+std::unordered_map<std::string, std::string> build_map(std::ifstream& map_file)
+{
+    std::unordered_map<std::string, std::string> trans_map;
+
+    std::string key;
+    std::string value;
+    
+    // read the first word into key and the rest into value
+    while(map_file >> key && std::getline(map_file, value))
+    {
+        if (value.size() > 1)
+            trans_map[key] = value.substr(1);
+        else
+            continue;
+            //throw std::runtime_error("no rule for " + key);
+            
+    }
+
+    return trans_map;
+}
+
+const std::string& 
+_transform(const std::string&s, const std::unordered_map<std::string, std::string>& m)
+{
+    auto it = m.find(s);
+    if (it != m.cend())
+        return it->second;
+    else
+        return s;
+    
+}
+
+void word_transform(std::ifstream& map_file, std::ifstream& input)
+{
+    auto trans_map = build_map(map_file);
+
+    std::string text;
+    while (std::getline(input, text))
+    {
+        std::istringstream stream(text);
+        
+        std::string word;
+        bool is_first = true;
+        while (stream >> word)
+        {
+            if (is_first)
+                is_first = false;
+            else
+                std::cout << " ";
+            
+            std::cout << _transform(word, trans_map);
+        }
+        std::cout << std::endl;
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    std::ifstream map_file(argv[1]);
+    std::ifstream input(argv[2]);
+    word_transform(map_file, input);
+}

323012dbee0be335da8cf626ae9fd292d0cbf00f
Kohei Hagiwara <kohei.hagiwara@stjude.org>
2024-03-16 00:34:08 -0500

    day105

--- chapter_12/section_12.1/example_page_467.cpp
+++ chapter_12/section_12.1/example_page_467.cpp
@@ -1,3 +1,28 @@
 #include <iostream>
 #include <memory>
 
+
+void f(std::shared_ptr<int> p)
+{
+    std::cout << p.use_count() << std::endl;
+    *p = 505; 
+    if (true) throw std::runtime_error(
+                        "something is wrong.");
+}
+
+int main()
+{
+    auto p = std::make_shared<int>(67);
+    std::cout << p.use_count() << std::endl;
+
+    try
+    {
+        f(p);
+    }
+    catch (...)
+    {
+        std::cout << p.use_count() << " " << *p << std::endl;
+    }
+
+    return 0;
+}

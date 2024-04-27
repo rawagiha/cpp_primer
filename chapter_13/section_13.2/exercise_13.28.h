0be8e650ff4cf9048ec1d0a0b99fa258ab896280
Kohei Hagiwara <kohei.hagiwara@stjude.org>
2024-04-26 23:53:46 -0500

    day118

---
+++ chapter_13/section_13.2/exercise_13.28.h
@@ -0,0 +1,15 @@
+#ifndef EX_13_28_H
+#define EX_13_28_H
+
+#include <string>
+
+class TreeNode
+{
+private:
+    std::string value
+    int count;
+    TreeNode* left;
+    TreeNode* right;
+};
+
+#endif

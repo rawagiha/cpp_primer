be7d96752e6d1e193e40d6c86372816bece7c933
Kohei Hagiwara <kohei.hagiwara@stjude.org>
2024-05-16 23:52:04 -0500

    day121

---
+++ chapter_16/section_16.1/exercise_16.4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <list>
+
+template <typename T, typename U> 
+T _find(const T& t, const U& u)
+{
+    while (t != u.end())
+    {
+    }
+
+}

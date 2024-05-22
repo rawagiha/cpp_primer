4b1241b7b280ad4c346b8c1b493246ceda7e2ddc
Kohei Hagiwara <kohei.hagiwara@stjude.org>
2024-05-21 23:39:56 -0500

    day123

--- chapter_13/section_13.4/example_page_521.cpp
+++ chapter_13/section_13.4/example_page_521.cpp
@@ -8,6 +8,13 @@ class Message
 public:
     explicit Message(const std::string& str = "") :
         contents(str) { }
+    
+    Message(const Message&);
+    Message& operator = (const Message&);
+    ~Message();
+    
+    void save(Folder&);
+    void remove(Folder&);
 private:
     std::string contents;
 };

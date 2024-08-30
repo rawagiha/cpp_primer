ae19dce13c508d88021c873d0eb1346a64e69826
Kohei Hagiwara <kohei.hagiwara@stjude.org>
2024-08-30 13:08:58 -0500

    day131

--- algorithms_otsuki/chapter_01/exercise/exercise_1.2.cpp
+++ algorithms_otsuki/chapter_01/exercise/exercise_1.2.cpp
@@ -1,23 +1,54 @@
 #include <iostream>
+#include <numeric>
+#include <vector>
 
 bool is_satiffied(const int i, const int j, const int k)
 {
     if (!k) return false;
     
-    int a = (20 + i) * j
+    int a = (20 + i) * j;
     
     //tens digit of a must be 3
     if ((a / 10) % 10 != 3) return false;
 
-    int b = (20 * i) * (k * 10);
+    int b = (20 + i) * (k * 10);
 
-    if (b < 100) return false;
+    if ((b < 100) || (b > 999)) return false;
 
     int c = a + b;
-
+    
+    if (c > 999) return false;
+    
     if ((c / 10) % 10 != 4) return false;
-
-    return true;
 }
 
+void show(const int i, const int j, const int k)
+{
+    std::cout << " " << 2 << i << std::endl;
+    std::cout << "x" << k << j << std::endl;
+    std::cout << "---" << std::endl;
+    std::cout << (20 + i) * j << std::endl;
+    std::cout << (20 + i) * (k * 10) << std::endl;
+    std::cout <<  "---" << std::endl;
+    std::cout <<  (20 + i) * j + (20 + i) * (k * 10) << std::endl;
+}
+
+int main()
+{
+    std::vector<int> numbers(10);
+    std::iota(numbers.begin(), numbers.end(), 0);
+
+    //brute force solution
+    for (int i : numbers)
+    {
+        for (int j : numbers)
+        {
+            for (int k : numbers)
+            {
+                if (is_satiffied(i, j, k))
+                    show(i, j, k);
+            }
+        }
+    }
+}
 

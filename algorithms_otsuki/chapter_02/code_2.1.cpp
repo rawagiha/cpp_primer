1c6a40ef12a13055aaf56c66bf1f464115c96884
Kohei Hagiwara <kohei.hagiwara@stjude.org>
2024-09-06 10:45:20 -0500

    day132

---
+++ algorithms_otsuki/chapter_02/code_2.1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <chrono>
+
+int main()
+{
+    int n;
+    std::cin >> n;
+    
+    if (!n)
+    {
+        std::cout << "invalid input" << std::endl;
+        return 1;
+    }
+    
+    int count = 0;
+
+    auto t1 = std::chrono::high_resolution_clock::now();
+    for (int i = 0; i < n; ++i)
+    {
+        ++count;
+    }
+    auto t2 = std::chrono::high_resolution_clock::now();
+    
+    auto ms_int = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1);
+ 
+    std::cout << ms_int.count() << " micro seconds" << std::endl;
+    
+    return 0;
+}

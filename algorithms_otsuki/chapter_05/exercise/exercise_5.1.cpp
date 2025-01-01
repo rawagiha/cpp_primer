72ec2dae6b31d5c56dd2c10dae7c62ac9090a96d
Kohei Hagiwara <kohei.hagiwara@stjude.org>
2024-12-31 23:48:53 -0600

    day155

--- algorithms_otsuki/chapter_05/exercise/exercise_5.1.cpp
+++ algorithms_otsuki/chapter_05/exercise/exercise_5.1.cpp
@@ -17,4 +17,51 @@ void fill_matrix(std::vector<std::vector<int>>& m)
 }
 
 
+size_t get_larger(const std::vector<int>& v, const size_t i)
+{
+    size_t j = 0, k = 0;
+    if (i == 0)
+    {
+        j = 1;
+        k = 2;
+    }
+    else if (i == 1)
+    {
+        j = 0;
+        k = 2;
+    }
+    else
+    {
+        j = 0;
+        k = 1;
+    }
 
+    if (v[j] < v[k]) return k;
+    else return j;
+}
+
+
+int main()
+{
+    int N;
+    std::cin >> N;
+    
+    std::vector<std::vector<int>> happiness(N, std::vector<int>(3));
+
+    fill_matrix(happiness);
+    
+    auto i = std::distance(
+                happiness[0].begin(), 
+                std::max_element(happiness[0].begin(), happiness[0].end())
+    );
+    
+    int total_happniness = happiness[0][i];
+
+    for (size_t n = 1; n < N; ++n)
+    {
+        i = get_larger(happiness[n], i);
+        total_happniness += happiness[n][i];
+    } 
+
+    std::cout << total_happniness << std::endl;
+}

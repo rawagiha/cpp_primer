0a97e87263deb1087eed1702291af5388289bb59
Kohei Hagiwara <kohei.hagiwara@stjude.org>
2025-05-10 17:19:50 -0500

    day160

---
+++ algorithms_otsuki/chapter_09/code_9.1.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <vector>
+
+const int MAX = 3;
+
+int st[MAX];
+int top = 0;
+
+void init()
+{
+    top = 0;
+}
+
+bool is_empty()
+{
+    return (top == 0);
+}
+
+bool is_full()
+{
+    return (top == MAX);
+}
+
+void push(int x)
+{
+    if (is_full())
+    {
+        std::cout << "error: stack is full." << std::endl;
+        return;
+    }
+
+    st[top] = x;
+    ++top;
+}
+
+int pop()
+{
+    if (is_empty())
+    {
+        std::cout << "error: stack is empty." << std::endl;
+        return -1;
+    }
+    --top;
+    return st[top];
+}
+
+void show(int st_[], int top_)
+{
+    for (int i = 0; i < top_; ++i)
+    {
+        std::cout << st_[i] << ", ";
+    }
+    std::cout << std::endl;
+}
+
+int main()
+{
+    init();
+
+    push(3);
+    push(5);
+    push(7);
+    show(st, top); 
+
+    push(9);
+
+    pop();
+    pop();
+    show(st, top);
+    pop();
+    pop();
+}
